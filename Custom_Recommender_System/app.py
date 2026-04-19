57893c5e3c7d8c17c126dbfa18002f53430cd003
Bhavik <bhavikfulfagar1008@gmail.com>
2026-04-20 02:48:23 +0530

    Clean project without large files

--- Custom_Recommender_System/app.py
+++ Custom_Recommender_System/app.py
@@ -1,29 +1,45 @@
 import streamlit as st
-import pickle
 import pandas as pd
+import requests
+import pickle
 
+# 🔥 Cache function (important)
+@st.cache_data
+def load_pickle(url):
+    response = requests.get(url)
+    return pickle.loads(response.content)
 
 st.title("Movie Recommender")
 
-movies = pickle.load(open('movies.pkl','rb'))
+# 🔗 Load data from Google Drive
+movies = load_pickle("https://drive.google.com/uc?id=1eL4DCggPcYJGOC5Zod9vVnwKs1TwqNRp")
 movies = pd.DataFrame(movies)
-similarity=pickle.load(open('similarity.pkl','rb'))
 
-selected_movie = st.selectbox('Which movie do you like ?',(movies['title'].values))
+similarity = load_pickle("https://drive.google.com/uc?id=1jr82QsZlLIrTePniLrc1mYjw81xf0Er2")
 
+# 🎯 UI
+selected_movie = st.selectbox(
+    'Which movie do you like ?',
+    movies['title'].tolist()
+)
+
+# 🧠 Recommendation logic
 def recommend(movie):
-    movie_idx=movies[movies['title']==movie].index[0]
-    recommend_movies=[]
-    movie_list=sorted(list(enumerate(similarity[movie_idx])),reverse=True,key=lambda x:x[1])[1:6]
+    movie_idx = movies[movies['title'] == movie].index[0]
+    
+    movie_list = sorted(
+        list(enumerate(similarity[movie_idx])),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
 
-    for i in movie_list:
-        recommend_movies.append(movies.iloc[i[0]].title)
-    return recommend_movies
+    return [movies.iloc[i[0]].title for i in movie_list]
 
+# 🎬 Output
 st.subheader("Recommended Movies:")
 
 if st.button("Recommend"):
-    recommendations=recommend(selected_movie)
+    recommendations = recommend(selected_movie)
 
     for movie in recommendations:
         st.write(movie)
